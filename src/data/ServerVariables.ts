cb0fe3c89c775cb8621e45850129624a14fd4588
Jack Valladares <52551151+JackValladares@users.noreply.github.com>
2025-02-27 17:22:49 -0500

    HTTPS

--- src/data/ServerVariables.ts
+++ src/data/ServerVariables.ts
@@ -1,4 +1,4 @@
-export const API_URL: string = "http://159.65.40.109:4000";
+export const API_URL: string = "https://159.65.40.109:4000";
 
 //In seconds
 export const BACKEND_PING_FREQUENCY: number = 100;
