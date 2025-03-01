1bfe001629457f26bfede6717ec10247c24faa68
Jack Valladares <52551151+JackValladares@users.noreply.github.com>
2025-02-28 21:39:24 -0500

    Server Ping Time Change

--- src/data/ServerVariables.ts
+++ src/data/ServerVariables.ts
@@ -1,5 +1,5 @@
 export const API_URL: string = "https://159.65.40.109:4000";
 
 //In seconds
-export const BACKEND_PING_FREQUENCY: number = 100;
-export const BACKEND_UPDATE_FREQUENCY: number = 500;
+export const BACKEND_PING_FREQUENCY: number = 1000;
+export const BACKEND_UPDATE_FREQUENCY: number = 2000;
